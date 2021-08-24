a33b6cde90a3b50b532b070f51883ed88c959dbd
Doring <d0211468@kowon.dongseo.ac.kr>
2021-08-24 15:47:33 +0900

    ObjData

---
+++ 2D RPG 실습 파일/ObjData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjData : MonoBehaviour
+{
+    public int id; // 오브젝트 ID
+    public bool isNpc; // NPC 여부 확인용 변수
+
+}
